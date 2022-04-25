5eeaf298dd6ab8cab8cb83add8d32d148cddaaf3
Hlib.Maksymenko <hmaksymenko@deloitte.com>
2022-04-26 00:09:14 +0300

    new config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
+console.log('Starting server...');
 import express from 'express';
 import http from 'http';
 import socketIo from 'socket.io';
 import { stream } from './ws/stream';
+import path from 'path'
 let app = express();
 let server = new http.Server(app);
 export const io = new socketIo.Server(server, { cors: { origin: '*' } });
+app.use('/', express.static(path.join(__dirname, 'public/app')));
 
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: path.join(__dirname, 'public/app') });
+});
 io.on('connection', stream);
 
 server.listen(3000);
-
+console.log('Server started)');
